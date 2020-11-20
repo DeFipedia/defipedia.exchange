028d5c745fc7a07031d6a21d7bc7984dfbeb6932
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-11-19 20:14:05 -0500

    successfull attempt with useWallet

--- src/App.js
+++ src/App.js
@@ -7,68 +7,88 @@ import Details from './pages/Details';
 import {connectAuction} from './functions/connectAuction'
 import {getPoolData} from './functions/getPoolData'
 import {web3Enabled} from './functions/web3Enabled'
-const App = () => {
-  
-  
+import { useWallet, UseWalletProvider } from 'use-wallet'
+import Web3 from "web3";
+
+function App () {  
   // state variables//
   const [tokenData, setTokenData] = useState({})
   const [poolData, setPoolData] = useState({})
   const [accountAddr, setAccountAddr] = useState(null)
 
-  const checkWalletConnection = async () => {
-    let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    let selectedAccount = accounts[0]        
-    setAccountAddr(selectedAccount)
-  }
+  // const checkWalletConnection = async () => {
+  //   let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  //   let selectedAccount = accounts[0]        
+  //   setAccountAddr(selectedAccount)
+  // }
 
-  const fetchData = async () => {
-    if(web3Enabled) {
-      let currentTokenData = await connectAuction()
-      setTokenData(currentTokenData)
-      let currentPoolData = await getPoolData()
-      setPoolData(currentPoolData)
-    }else{
-      alert('Please install a Ethereum-compatible browser or extension like MetaMask to use this dApp')
-    }
-  }
+  // const fetchData = async () => {
+  //   if(web3Enabled) {
+  //     let currentTokenData = await connectAuction()
+  //     setTokenData(currentTokenData)
+  //     let currentPoolData = await getPoolData()
+  //     setPoolData(currentPoolData)
+  //   }else{
+  //     alert('Please install a Ethereum-compatible browser or extension like MetaMask to use this dApp')
+  //   }
+  // }
 
-  const connectWallet = async () => {
-      if(web3Enabled) {
-        let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-        let selectedAccount = accounts[0]        
-        setAccountAddr(selectedAccount)
-      }
-  }
+  // const connectWallet = async () => {
+  //     if(web3Enabled) {
+  //       let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  //       let selectedAccount = accounts[0]        
+  //       setAccountAddr(selectedAccount)
+  //     }
+  // }
   //connecting to auction, checking for wallet connection //
-  useEffect(() => {
-    fetchData()
-    checkWalletConnection()
-  }, [])
-  
+  // useEffect(() => {
+  //   fetchData()
+  //   checkWalletConnection()
+  // }, [])
+
+
+  //attempt to aragon connect use-wallet//
+  const wallet = useWallet()
+
+  const connectWallet = async () =>  {
+    await wallet.connect()
+    console.log(wallet.account)
+  }
+
   // -----------------------//
   return (
       <div className='App'>
-      <Navbar 
-        brandTitle={process.env.PUBLIC_URL + 'assets/brand-title.svg'}
-        brandLogo={process.env.PUBLIC_URL + 'defipedia_logo.png'}
-        accountAddr={accountAddr}
-        connectWallet={connectWallet} 
-      />
-      <Switch>
-        <Route exact path='/'>
-          <Home 
-            tokenData={tokenData} 
-            poolData={poolData} 
-            accountAddr={accountAddr}
-            connectWallet={connectWallet}
-          />
-        </Route>
-        <Route path='/details'>
-          <Details tokenData={tokenData} poolData={poolData} />
-        </Route>
-      </Switch>
-    </div>
+        <Navbar 
+          brandTitle={process.env.PUBLIC_URL + 'assets/brand-title.svg'}
+          brandLogo={process.env.PUBLIC_URL + 'defipedia_logo.png'}
+          accountAddr={accountAddr}
+          connectWallet={connectWallet} 
+        />
+        <Switch>
+          <Route exact path='/'>
+            <Home 
+              tokenData={tokenData} 
+              poolData={poolData} 
+              accountAddr={accountAddr}
+              // connectWallet={connectWallet}
+            />
+          </Route>
+          <Route path='/details'>
+            <Details tokenData={tokenData} poolData={poolData} />
+          </Route>
+        </Switch>
+      </div>
   );
 }
 
-export default App;
+export default () => (
+  <UseWalletProvider
+    chainId={1}
+    connectors={{
+      // This is how connectors get configured
+      portis: { dAppId: '47c91e55-8ecf-45f6-937d-9015bcf65b36' },
+    }}
+  >
+  <App />
+  </UseWalletProvider> 
+) 
