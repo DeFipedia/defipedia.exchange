93b9177dbab851789f0258bb9c13b675be2d8841
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-11-25 17:52:24 -0500

    added layout for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Card, CardContent, Tooltip, Button, useTheme} from '@material-ui/core'
+import {Card, CardContent, Tooltip, Button, useTheme, Grid, TextField} from '@material-ui/core'
 import Popup from '../components/Popup'
 import CardModal from '../components/CardModal'
 import BuyModal from '../components/BuyModal'
@@ -48,23 +48,56 @@ const Home = (props) => {
 
     return(
         <div className='home-page'>
-            <AuctionCard 
-                title='Uniswap'
-                image={process.env.PUBLIC_URL + 'assets/cover-art.jpg'}
-                price={poolData.rate}
-                totalTokens='50'
-                learnMoreTag='Swap here!'
-            />
-            {/*@DEV: The auction-card element is used as masking layer*/}
-            <div className='auction-card-mask'>
-            </div>
-                <p className='auction-text'>Auction Closed</p>
+            <Grid container justify='center' spacing={1}>
+                <Grid item>
+                    {/* This is UniSwap Card */}
+                    <AuctionCard 
+                        title='Uniswap'
+                        image={process.env.PUBLIC_URL + 'assets/cover-art.jpg'}
+                        price={poolData.rate}
+                        totalTokens='50'
+                        learnMoreTag='Swap here!'
+                    />
+                </Grid>
+                <Grid item>
+                    {/* Sale Card */}
+                    <AuctionCard 
+                        title='Pre-sale'
+                        image={process.env.PUBLIC_URL + 'assets/books-presale.png'}
+                        price='dummy price'
+                        totalTokens='950'
+                        learnMoreTag='Buy direct!'
+                    />
+                </Grid>
+                {/*@DEV: The auction-card element is used as masking layer*/}
+                {/* <div className='auction-card-mask'>
+                    </div>
+                    <p className='auction-text'>Auction Closed</p> */}
                 {/* This blob until here from last comment is used as masking layer for Auction card(old dutch aution car) */}
-                <AuctionCard 
-                    title='Auction'
-                    image={process.env.PUBLIC_URL + 'assets/cover-art.jpg'}
-                    totalTokens='950'
-                />
+                <Grid item>
+                    <AuctionCard 
+                        title='Auction'
+                        image={process.env.PUBLIC_URL + 'assets/cover-art.jpg'}
+                        totalTokens='950'
+                    />
+                </Grid>
+            </Grid>
+            {/* @DEV: This is for rendering footer */}
+            <footer>
+                <p>Don't miss out, keep pace with all the latest</p>
+                <span>
+                    <input placeholder='Add an email address' />
+                    <Button variant='comtained'>Subscribe</Button>
+                </span>
+                <section>
+                    <i class="fab fa-discord"></i>
+                    <i class="fab fa-twitter-square"></i>
+                    <i class="fab fa-linkedin"></i>
+                    <i class="fab fa-github-square"></i>
+                    <i class="fab fa-telegram"></i>
+                    <i class="fab fa-medium"></i>
+                </section>
+            </footer>
         </div>
     )
 }
