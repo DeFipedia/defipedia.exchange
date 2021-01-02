c17f817cea8ddc13ca345632cc2fead5e2e37ca4
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-01-01 23:08:40 -0500

    handling state for email input in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button} from './Button'
 
 export const Footer = () => {
 
+    const [email, setEmail] = useState('')
+
+    const handleInputChange = (e) => {
+        let {value} = e.target
+        setEmail(value)
+    }
     return(
         <footer>
             <p>Don't miss out, keep pace with all the latest</p>
             <span>
-                <input placeholder='Add an email address' />
+                <input placeholder='Add an email address' type='text' value={email} onChange={(e) => handleInputChange(e)} />
                 {/* this is MUI button, not the custom one */}
                 {/* <Button variant='outlined'>Subscribe</Button> */}
                 <Button label='Subscribe' variant='outlined' size='small' />
