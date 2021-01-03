a51b51f036744bd96b705cab423cb64c539c6e09
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-01-02 20:16:22 -0500

    passing email to subscribe function dektop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { subscribeToNewsletter } from '../functions/subscribeToNewletter'
 import {Button} from './Button'
 
 export const Footer = () => {
@@ -9,14 +10,18 @@ export const Footer = () => {
         let {value} = e.target
         setEmail(value)
     }
+
+    const submitEmail = async () => {
+        subscribeToNewsletter(email)
+    }
+
     return(
         <footer>
             <p>Don't miss out, keep pace with all the latest</p>
             <span>
                 <input placeholder='Add an email address' type='text' value={email} onChange={(e) => handleInputChange(e)} />
                 {/* this is MUI button, not the custom one */}
-                {/* <Button variant='outlined'>Subscribe</Button> */}
-                <Button label='Subscribe' variant='outlined' size='small' />
+                <Button label='Subscribe' variant='outlined' size='small' onClick={submitEmail}/>
                 <i className="fab fa-discord"></i>
                 <i className="fab fa-twitter-square"></i>
                 <i className="fab fa-linkedin"></i>
