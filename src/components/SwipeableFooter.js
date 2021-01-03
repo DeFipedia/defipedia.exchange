6d7684eecae4c8083255f685c426306dcab9bc0d
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-01-03 18:40:08 -0500

    added UX behaviour when user hists subscribe on swipeable footer

--- src/components/SwipeableFooter.js
+++ src/components/SwipeableFooter.js
@@ -9,6 +9,8 @@ const SwipeableFooter = () => {
 
     const [open, setOpen] = useState(false)
 
+    const [emailSubmitted, setEmailSubmitted ] = useState(false)
+
     const handleInputChange = (e) => {
         let {value} = e.target
         setEmail(value)
@@ -16,22 +18,37 @@ const SwipeableFooter = () => {
 
     const submitEmail = async () => {
         if(email !== ''){
-            subscribeToNewsletter(email)
+            await subscribeToNewsletter(email)
+            setEmailSubmitted(true)
         }
     }
 
-    const openSwipeableView = () => {
+    const toggleSwipeableView = () => {
         setOpen(!open)
     }
+
+    // if(emailSubmitted) {
+    //     return <p>thitititititititit</p>
+    // }
     return(
         <SwipeableBottomSheet overflowHeight={60} open={open}>
             <div className='swipeable-footer' style={{ height: '285px' }} >
-                <div className='nugget' onClick={openSwipeableView}></div>
-                <p>Don't miss out, keep pace with all the latest</p>
-                <section className='subscribe-section'>
-                    <input placeholder='Add an email address' value={email} onChange={(e) => handleInputChange(e)} required/>
-                    <Button variant='outlined' size='medium' onClick={submitEmail}>Subscribe</Button>         
-                </section>
+                <div className='nugget' onClick={toggleSwipeableView}></div>
+                { emailSubmitted ?
+                    <React.Fragment>
+                        <p>Awesome! You are subscribed to our newsletter:</p>
+                        <p>{email}</p>
+                        <Button variant='outlined' size='medium' onClick={toggleSwipeableView}>Back to $BOOKS</Button> 
+                    </React.Fragment>
+                    :
+                    <React.Fragment>
+                        <p>Don't miss out, keep pace with all the latest</p>
+                        <section className='subscribe-section'>
+                            <input placeholder='Add an email address' value={email} onChange={(e) => handleInputChange(e)} required/>
+                            <Button variant='outlined' size='medium' onClick={submitEmail}>Subscribe</Button>         
+                        </section>
+                    </React.Fragment>
+                }
                 <section className='social-media-handle'>
                     <a href='https://discord.com/invite/vNBe7CC' target='_blank' rel='noopener noreferrer'>
                         <i className='fab fa-discord'></i>
